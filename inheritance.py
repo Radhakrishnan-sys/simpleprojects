d47a1cdc7623adfa03f693a364f7565f52744d5a
sasilab <sasistartup@gmail.com>
2025-04-09 16:30:02 +0200

    my new inheritance example

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+class Car :
+    def __init__ (self, name, model , year, color):
+        self.name = name
+        self.model = model
+        self.year = year
+        self.color = color
+        self.engine = True
+
+    def service(self):
+        print(f"{self.name} is currently running")
+
+class Maruthi(Car):
+    pass
+
+class Benz(Car):
+    pass
+
+maruthi = Maruthi ("omni", "k310", "1992", "Pink")
+
+print(maruthi.name)
+
+maruthi.service()
